07c3e8a4cb394d91c52866a1820bc3f6d4fccc88
ana <ana_alcalde1@hotmail.com>
2016-12-19 13:07:29 +0100

    Añadir colores con opciones

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
+"use strict"
 console.log("Repasando JavaScript")
 
-
-var paragraph = document.querySelector("div");
-paragraph.innerHTML = "He aprendido bien cómo funcionan los bucles";
-var frase = "He aprendido bien cómo funcionan los bucles"
-var resultado = "<p>" + "He aprendido bien cómo funcionan los bucles" + "</p>"
-
+var paragraph = document.querySelector("div")
+paragraph.innerHTML = "He aprendido bien cómo funcionan los bucles"
+var resultado = ""
+var frase = "<p>He aprendido bien cómo funcionan los bucles</p>"
+var elegirColor= "<select> <option>blanco</option><option>azul</option><option>rojo</option> <option>verde</option><option>amarillo</option> <option>rosa</option></select>"
 
 for (var i = 0; i < 100; i++) {
 
-  frase = frase +  resultado
+  resultado = resultado + frase + elegirColor
 
-  console.log (frase)
 }
 
- paragraph.innerHTML = frase
+paragraph.innerHTML = resultado
 
+ 
