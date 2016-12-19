a83c45198672ea4fe220c52aced146bb8ef490fc
Noemí <noemi.pacheco.martinez@gmail.com>
2016-12-19 09:56:06 +0100

    Añadida la funcionalidad que pinta en la pizarra 100 veces un mensaje

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 console.log("Repasando JavaScript")
 
-var paragraph = document.querySelector("p");
+
+var paragraph = document.querySelector("div");
 paragraph.innerHTML = "He aprendido bien cómo funcionan los bucles";
+var frase = "He aprendido bien cómo funcionan los bucles"
+var resultado = "<p>" + "He aprendido bien cómo funcionan los bucles" + "</p>"
+
+
+for (var i = 0; i < 100; i++) {
+
+  frase = frase +  resultado
+
+  console.log (frase)
+}
+
+ paragraph.innerHTML = frase
+
