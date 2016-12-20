0fa53acfa43ab140275f853a1876ef0ed451ae6f
ana <ana_alcalde1@hotmail.com>
2016-12-20 08:29:35 +0100

    Pintar el primer parrafo según las opciones del select

--- index.js
+++ index.js
@@ -15,4 +15,24 @@ for (var i = 0; i < 100; i++) {
 
 paragraph.innerHTML = resultado
 
- 
+var selector=document.querySelector("select")
+
+function usuarioElige(event){
+  var numeros=event.target.selectedIndex
+  document.querySelector("p").style.color="red"
+  if (numeros===0) {
+    document.querySelector("p").style.color="white"
+  } else if(numeros===1){
+    document.querySelector("p").style.color="blue"
+  }else if(numeros===2){
+    document.querySelector("p").style.color="red"
+  }else if(numeros===3){
+    document.querySelector("p").style.color="green"
+  }else if(numeros===4){
+    document.querySelector("p").style.color="yellow"
+  }else{
+    document.querySelector("p").style.color="pink"
+  }
+}
+
+selector.addEventListener("change", usuarioElige)
